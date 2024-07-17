5db1627003e1eef75ba7aeb8983711de8e12ea9b
sigmanotation <zachserikow@gmail.com>
2024-07-17 15:45:49 -0400

    Time corrections still WIP.

--- time_correction_calculator.py
+++ time_correction_calculator.py
@@ -5,17 +5,32 @@ from numba import njit
 
 import numpy as np
 import h5py as h5
+import lmfit
 
 NUM_CHANNELS = 10240
 THRESHOLD = 0.1
 
 # Configuration parameters
 workspace_path = Path("/Volumes/e20009/test_pulser")
-trace_path = Path("/Volumes/e20009/pulser_h5")
-run = 382
+write = True
+write_path = Path("/Users/attpc/Desktop/teehee")
+
+pulser_info = {
+    372: 0.1,
+    373: 0.2,
+    374: 0.3,
+    375: 0.4,
+    376: 0.5,
+    377: 0.75,
+    378: 1.0,
+    379: 1.25,
+    380: 1.5,
+    381: 1.75,
+    382: 2.0,
+}
 
 
-def tc_calculator(workspace_path: Path, traces_path: Path, run: int):
+def run_tc_calculator(workspace_path: Path, run: int, write: bool, write_path: Path):
     """
     Determines the time correction of each pad using pulser runs. This function analyzes the
     point clouds produced from running pulser runs through Spyral. For each pad in a pulser
@@ -57,6 +72,7 @@ def tc_calculator(workspace_path: Path, traces_path: Path, run: int):
     # Result storage
     pad_tb = np.zeros((NUM_CHANNELS, num_events), dtype=np.int64)
     pad_hits = np.zeros((NUM_CHANNELS, num_events), dtype=np.int64)
+    pad_amp = np.zeros((NUM_CHANNELS, num_events))
 
     # Go through each event
     for idx in range(num_events):
@@ -72,14 +88,15 @@ def tc_calculator(workspace_path: Path, traces_path: Path, run: int):
             continue
 
         pc: np.ndarray = cloud_data[:].copy()
-        pads_event, hits_event = pad_times(pc)
-        pad_tb[:, idx] = pads_event
-        pad_hits[:, idx] = hits_event
+        event_tb, event_hits, event_amp = pad_times(pc)
+        pad_tb[:, idx] = event_tb
+        pad_hits[:, idx] = event_hits
+        pad_amp[:, idx] = event_amp
 
     # Make mask array masking pads that have many zero hits in an event
     mask = np.sum(pad_hits, axis=1) < 0.1 * num_events
-    mask = np.tile(mask, (num_events, 1)).T
-    pad_tb_ma = np.ma.array(pad_tb, mask=mask)
+    mask_array = np.tile(mask, (num_events, 1)).T
+    pad_tb_ma = np.ma.array(pad_tb, mask=mask_array)
 
     # Find pad's average tb
     pad_tb_avg = np.ma.mean(pad_tb_ma, axis=1)
@@ -90,17 +107,24 @@ def tc_calculator(workspace_path: Path, traces_path: Path, run: int):
     run_tb_avg = np.ma.average(pad_tb_avg, weights=pad_tb_weights)
     run_tb_err = np.ma.sqrt(1 / np.ma.sum(pad_tb_err**-2))
 
-    # Fint run's time bucket correction factors
+    # Find run's time bucket correction factors
     tb_factors = run_tb_avg - pad_tb_avg
     tb_factors_err = np.ma.sqrt(run_tb_err**2 + pad_tb_err**2)
     tb_factors = tb_factors.filled(0.0)
     tb_factors_err = tb_factors_err.filled(0.0)
 
-    np.savetxt(
-        "/Users/attpc/Desktop/e20009_analysis/e20009_analysis/e20009_parameters/new2000mv.csv",
-        np.transpose([tb_factors, tb_factors_err]),
-        fmt="%.4f",
-    )
+    # Find average amplitude of each pad
+    pad_amp_avg = np.mean(pad_amp, axis=1)
+    pad_amp_avg[mask] = 0.0
+
+    if write is True:
+        np.savetxt(
+            write_path / f"{form_run_string(run)}_time_correction.csv",
+            np.transpose([tb_factors, tb_factors_err, pad_amp_avg]),
+            fmt="%.4f",
+        )
+
+    return tb_factors, tb_factors_err, pad_amp_avg
 
 
 @njit
@@ -130,30 +154,73 @@ def pad_times(pc: np.ndarray):
     """
 
     # Result storage for event
-    pads_event = np.zeros(NUM_CHANNELS, dtype=np.int64)
-    hits_event = np.zeros(NUM_CHANNELS, dtype=np.int64)
+    event_tb = np.zeros(NUM_CHANNELS, dtype=np.int64)
+    event_hits = np.zeros(NUM_CHANNELS, dtype=np.int64)
+    event_amp = np.zeros(NUM_CHANNELS, dtype=np.int64)
 
     for point in pc:
 
         pad_id = int(point[5])
         tb = int(point[6])
+        amp = point[3]
 
         if (
-            pads_event[pad_id] == 0
+            event_tb[pad_id] == 0
         ):  # Array is initialized with 0, so we always want to replace the first time a pad is encountered
-            pads_event[pad_id] = tb
-            hits_event[pad_id] += 1
+            event_tb[pad_id] = tb
+            event_amp[pad_id] = amp
+            event_hits[pad_id] += 1
 
         elif (
-            tb < pads_event[pad_id]
+            tb < event_tb[pad_id]
         ):  # Only take the smallest point from each trace in an event
-            pads_event[pad_id] = tb
+            event_tb[pad_id] = tb
+            event_amp[pad_id] = amp
 
-    return pads_event, hits_event
+    return event_tb, event_hits, event_amp
+
+
+def tc_linear_fit(
+    pulser_info: dict,
+    workspace_path: Path,
+    write: bool,
+    write_path: Path,
+):
+
+    all_tc_factors = np.zeros((NUM_CHANNELS, len(pulser_info)))
+    all_tc_err_factors = np.zeros((NUM_CHANNELS, len(pulser_info)))
+    all_pad_amp = np.zeros((NUM_CHANNELS, len(pulser_info)))
+    results = np.zeros((NUM_CHANNELS, 2))
+
+    for idx, (run, voltage) in enumerate(pulser_info.items()):
+        tc_factor, tc_factor_err, pad_amp = run_tc_calculator(
+            workspace_path, run, write, write_path
+        )
+        all_tc_factors[:, idx] = tc_factor
+        all_tc_err_factors[:, idx] = tc_factor_err
+        all_pad_amp[:, idx] = tc_factor_err
+
+    for idx, (tc, err, amp) in enumerate(
+        zip(all_tc_factors, all_tc_err_factors, all_pad_amp)
+    ):
+        pass
+        # print(amp)
+        # linear_fit = lmfit.models.LinearModel()
+        # weights = 1.0 / np.sqrt(err)
+        # weights[err == 0.0] = 1.0
+        # pars = linear_fit.guess(x=list(pulser_info.values()), data=tc, weights=weights)
+        # fit_result = linear_fit.fit(
+        #     params=pars, x=list(pulser_info.values()), data=tc, weights=weights
+        # )
+
+        # results[idx, 0] = fit_result.params["slope"]
+        # results[idx, 1] = fit_result.params["intercept"]
+
+    # print(results)
 
 
 def main():
-    tc_calculator(workspace_path, trace_path, run)
+    tc_linear_fit(pulser_info, workspace_path, write, write_path)
 
 
 if __name__ == "__main__":
