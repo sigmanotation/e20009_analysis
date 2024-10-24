21ac448484f60120a191dab45827178ff6f22800
sigmanotation <zachserikow@gmail.com>
2024-10-24 16:27:23 -0400

    Big changes to way efficiency factors are found. Should use kinematics file directly which I know do in the simulated angles calculator. I still have the z gate in it which I may remove if I do Gordon's method. Now I just count the counts and dont fit the simulated data to find the factors. I maybe should add a very light gate on the redchisq.

--- simulations/simulated_angles_calculator.py
+++ simulations/simulated_angles_calculator.py
@@ -8,15 +8,9 @@ from spyral_utils.nuclear import NucleusData
 
 # Configuration parameters
 kine_path = Path(
-    "E:\\engine_v0.3.0\\inelastic_3.368mev_0-180cm\\inelastic_3.368.parquet"
+    "/Volumes/e20009_sim/engine_v0.3.0/elastic_0-180cm/elastic_kine.parquet"
 )
-sim_directory = Path(
-    "E:\\engine_v0.3.0\\inelastic_3.368mev_0-180cm\\workspace\\PointcloudLegacy"
-)
-write_path = Path("E:\\engine_v0.3.0\\inelastic_3.368mev_0-180cm")
-
-run_min = 1
-run_max = 19
+write_path = Path("/Volumes/e20009_sim/engine_v0.3.0/elastic_0-180cm")
 
 # Define reaction
 target: NucleusData = nuclear_map.get_data(1, 2)
@@ -24,22 +18,13 @@ beam: NucleusData = nuclear_map.get_data(4, 10)
 product: NucleusData = nuclear_map.get_data(4, 10)
 
 # Specify analysis gates
-vertex_z_min = 0.020  # Units of meters
+vertex_z_min = 0.004  # Units of meters
 vertex_z_max = 0.700  # Units of meters
 
 
-def find_sim_cm(
-    kine_path: Path,
-    sim_directory: Path,
-    run_min: int,
-    run_max: int,
-    target: NucleusData,
-    beam: NucleusData,
-    product: NucleusData,
-):
+def find_sim_cm(kine_path: Path):
     """
-    Finds the center of mass scattering angle of the input product in all the simulated events
-    that survived the detector effects.
+    Finds the center of mass scattering angle of the input product in all the simulated events.
 
     Parameters
     ----------
@@ -61,19 +46,12 @@ def find_sim_cm(
         List of [Z, A] of product nucleus whose center of mass scattering angle will
         be found.
     """
-    # Find events that survived applying detector effects
-    events = np.empty(0, int)
-    for run in range(run_min, run_max + 1):
-        cloud_f = None
-        path = sim_directory / f"run_{run:04d}.h5"
-        if not path.exists():
-            print(f"{run} does not exist in the input directory!")
-            continue
-        cloud_f = h5.File(path, "r")
 
-        events_run = np.array(list(cloud_f["cloud"]))
-        events_run = np.char.strip(events_run, chars="cloud_").astype(int)
-        events = np.append(events, events_run)
+    # Load in all simulated events
+    kine_f = pl.scan_parquet(kine_path)
+    # kine_f = kine_f.filter(
+    #     (vertex_z_min <= pl.col("vertex_z")) & (pl.col("vertex_z") <= vertex_z_max)
+    # )
 
     target_vector = vector.array(
         {
@@ -82,14 +60,6 @@ def find_sim_cm(
             "pz": [0.0],
             "E": [target.mass],
         }
-    )
-
-    # Find events in kinematics file that survived applying detector effects and vertex z-coordinate gate
-    kine_f = pl.scan_parquet(kine_path)
-    kine_f = kine_f.filter(
-        pl.col("event").is_in(events)
-        & (pl.col("vertex_z") >= vertex_z_min)
-        & (pl.col("vertex_z") <= vertex_z_max)
     )
 
     # beam_coords = (
@@ -140,12 +110,6 @@ def find_sim_cm(
         }
     )
 
-    # Not in use
-    # # Apply analysis excitation energy gate
-    # mask: np.ndarray = (product_mass_low <= product_vectors.mass) & (
-    #     product_vectors.mass < product_mass_high
-    # )
-
     cm_ang_det = product_vectors.boostCM_of(beam_vectors + target_vector).theta
 
     np.save(write_path / "cm_ang.npy", cm_ang_det)
@@ -154,10 +118,4 @@ def find_sim_cm(
 if __name__ == "__main__":
     find_sim_cm(
         kine_path,
-        sim_directory,
-        run_min,
-        run_max,
-        target,
-        beam,
-        product,
     )
