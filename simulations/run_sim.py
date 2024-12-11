656fafe0d2409b9e0916020a3abdf9687861c3b4
sigmanotation <zachserikow@gmail.com>
2024-12-11 15:50:27 -0500

    Adding latest plotting notebook and run sim and run spyral scripts.

--- simulations/run_sim.py
+++ simulations/run_sim.py
@@ -25,15 +25,15 @@ import numpy as np
 
 
 # Set output file paths for simulated kinematic events and point clouds
-kine_path = Path("E:\\newest\\dp_gs\\dp_gs_kine.h5")
-det_path = Path("E:\\newest\\dp_gs")
+kine_path = Path("E:\\newest\\inelastic_3.37\\inelastic_3.37_kine.h5")
+det_path = Path("E:\\newest\\inelastic_3.37")
 
 target = GasTarget(
     TargetData(compound=[(1, 2, 2)], pressure=600.0, thickness=None), nuclear_map
 )
 
 # Number of events to simulate
-nevents = 1500000
+nevents = 3000000
 
 # Deuteron break up pipeline
 # At least 2.22 MeV is needed to break up the deuteron.
@@ -62,41 +62,17 @@ nevents = 1500000
 #     ),
 # )
 
-# # 10Be(d,d) pipeline
-# pipeline = KinematicsPipeline(
-#     [
-#         Reaction(
-#             target=nuclear_map.get_data(1, 2),
-#             projectile=nuclear_map.get_data(4, 10),
-#             ejectile=nuclear_map.get_data(1, 2),
-#         )
-#     ],
-#     [
-#         ExcitationGaussian(3.37, 0.0),
-#         # ExcitationBreitWigner(
-#         #     rest_mass=nuclear_map.get_data(4, 11).mass,
-#         #     centroid=2.65,
-#         #     width=0.206,
-#         # )
-#     ],
-#     [PolarUniform(angle_min=0, angle_max=np.pi)],
-#     beam_energy=93.5,  # MeV
-#     target_material=KinematicsTargetMaterial(
-#         material=target, z_range=(0.0, 1.0), rho_sigma=0.02 / 3
-#     ),
-# )
-
-# 10Be(d,p) pipeline
+# 10Be(d,d) pipeline
 pipeline = KinematicsPipeline(
     [
         Reaction(
             target=nuclear_map.get_data(1, 2),
             projectile=nuclear_map.get_data(4, 10),
-            ejectile=nuclear_map.get_data(1, 1),
+            ejectile=nuclear_map.get_data(1, 2),
         )
     ],
     [
-        ExcitationGaussian(0.0, 0.0),
+        ExcitationGaussian(3.37, 0.0),
         # ExcitationBreitWigner(
         #     rest_mass=nuclear_map.get_data(4, 11).mass,
         #     centroid=2.65,
@@ -109,6 +85,30 @@ pipeline = KinematicsPipeline(
         material=target, z_range=(0.0, 1.0), rho_sigma=0.02 / 3
     ),
 )
+
+# # 10Be(d,p) pipeline
+# pipeline = KinematicsPipeline(
+#     [
+#         Reaction(
+#             target=nuclear_map.get_data(1, 2),
+#             projectile=nuclear_map.get_data(4, 10),
+#             ejectile=nuclear_map.get_data(1, 1),
+#         )
+#     ],
+#     [
+#         # ExcitationGaussian(0.32, 0.0),
+#         ExcitationBreitWigner(
+#             rest_mass=nuclear_map.get_data(4, 11).mass,
+#             centroid=3.40,
+#             width=0.122,
+#         )
+#     ],
+#     [PolarUniform(angle_min=0, angle_max=np.pi)],
+#     beam_energy=93.1,  # MeV
+#     target_material=KinematicsTargetMaterial(
+#         material=target, z_range=(0.0, 1.0), rho_sigma=0.02 / 3
+#     ),
+# )
 
 detector = DetectorParams(
     length=1.0,
